d1e9a880f5dca81e00c202e8be7a67627619f302
msats5 <saterson@gmail.com>
2018-04-14 19:29:09 -0400

    updated practice file

--- practice.lisp
+++ practice.lisp
@@ -1,5 +1,10 @@
-(defun multiply (x y)
-    (* x y))
+(defvar *counter* 0)
+(defvar *player-guess* '())
 
-(defun add (x y)
-    (+ x y))
+(defun Sequential (board colors SCSA last-response)
+    (declare (ignore SCSA last-response))
+        (if (= *counter* 100)
+            (setf *counter* 1)
+            (setf *counter* (1+ counter)))
+        (cond 
+            ((= counter 1) (setf *player-guess* (make-list board :initial-element 'A))))
