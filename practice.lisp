f06e6939cddfbaed5c9fa414507f3c063a7e8cc9
msats5 <saterson@gmail.com>
2018-04-14 20:25:05 -0400

    adding list conversion functions

--- practice.lisp
+++ practice.lisp
@@ -7,4 +7,14 @@
             (setf *counter* 1)
             (setf *counter* (1+ counter)))
         (cond 
-            ((= counter 1) (setf *player-guess* (make-list board :initial-element 'A))))
+            ((= counter 1) (setf *player-guess* (make-list board :initial-element '0))))
+
+)
+
+(defun list-to-int (x)
+    (loop for i from (1- (length x)) downto 0
+        collect (* (expt 10 (- (length x) (1+ i))) (nth i x)) into nums
+        finally (return (apply '+ nums))))
+
+(defun int-to-list (x)
+    )
