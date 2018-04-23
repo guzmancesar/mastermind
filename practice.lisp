3f410b654b7242e08469ec76cf333217dac99d73
msats5 <saterson@gmail.com>
2018-04-23 19:11:18 -0400

    fixed few errors in practice

--- practice.lisp
+++ practice.lisp
@@ -1,16 +1,16 @@
 (defvar *player-guess* '())
 
 (defun Sequential (board colors SCSA last-response)
-    (declare (ignore SCSA last-response))
+    (declare (ignore SCSA))
         (if (equal last-response nil)
-            (setf *player-guess* (make-list board :initial-element '0))
-            (increment *player-guest* colors)) ;do setf if this doesn't work
+            (setf *player-guess* (make-list board :initial-element 'A))
+            (increment *player-guess* colors)) ;do setf if this doesn't work
         ; return list of letters converted from *player-guess*
 )
 
 (defun increment (x colors)
     (loop for i from (1- (length x)) downto 0
-        if (= (nth i x) (first (last colors)))
+        if (equal (nth i x) (first (last colors)))
             do (setf (nth i x) (nth 0 colors))
         else 
             do (setf (nth i x) (nth (1+ (position (nth i x) colors)) colors)) 
