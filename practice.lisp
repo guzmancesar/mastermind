7c19120f784d251e781ca571c8aba3cc0934fd1f
msats5 <saterson@gmail.com>
2018-04-25 20:55:41 -0400

    finishing touches

--- practice.lisp
+++ practice.lisp
@@ -6,11 +6,11 @@
             (setf *sequential-guess* (make-list board :initial-element 'A))
             (increment *sequential-guess* colors)))
 
-(defun increment (x colors)
-    (loop for i from (1- (length x)) downto 0
-        if (equal (nth i x) (first (last colors)))
-            do (setf (nth i x) (nth 0 colors))
+(defun increment (guess colors)
+    (loop for i from (1- (length guess)) downto 0
+        if (equal (nth i guess) (first (last colors)))
+            do (setf (nth i guess) (nth 0 colors))
         else 
-            do (setf (nth i x) (nth (1+ (position (nth i x) colors)) colors)) 
-            and do (return x)
-        finally (return x)))
+            do (setf (nth i guess) (nth (1+ (position (nth i guess) colors)) colors)) 
+            and do (return guess)
+        finally (return guess)))
