2e90d777dc08bb0d5a2b30fa5b8b3eae231ec1f5
msats5 <saterson@gmail.com>
2018-05-07 12:38:32 -0400

    adjusted scsa scoring

--- scsa.lisp
+++ scsa.lisp
@@ -4,7 +4,7 @@
         (if (> color-number 2)
             ;; returns arbitrary bonus points (20) if current has no more than two colors
             (return-from two-color-alternating-scsa 0)
-            (return-from two-color-alternating-scsa 20))))
+            (return-from two-color-alternating-scsa 10))))
 
 (defun only-once-scsa (current)
     ;; checks if the current element has duplicates or not
@@ -12,7 +12,7 @@
         when (member e (rest (member e current)))
         ;; returns arbitrary bonus points (20) if no duplicates
         do (return-from only-once-scsa 0)
-        finally (return-from only-once-scsa 20)))
+        finally (return-from only-once-scsa 10)))
 
 (defun scsa-match (current SCSA)
     (cond ((eql SCSA 'two-color-alternating) (two-color-alternating-scsa current))
