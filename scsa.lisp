a4d6a03240a876050a697875e178805bbc02d954
msats5 <saterson@gmail.com>
2018-05-07 12:33:27 -0400

    added only-once checker

--- scsa.lisp
+++ scsa.lisp
@@ -1,11 +1,20 @@
 (defun two-color-alternating-scsa (current)
+    ;; this is really dumb (only checks number of colors, not for alternating), so can be updated
     (let ((color-number (length (remove-duplicates current))))
         (if (> color-number 2)
             ;; returns arbitrary bonus points (20) if current has no more than two colors
             (return-from two-color-alternating-scsa 0)
             (return-from two-color-alternating-scsa 20))))
 
+(defun only-once-scsa (current)
+    ;; checks if the current element has duplicates or not
+    (loop for e in current
+        when (member e (rest (member e current)))
+        do (return-from only-once-scsa nil)
+        finally (return-from only-once-scsa T)))
+
 (defun scsa-match (current SCSA)
     (cond ((eql SCSA 'two-color-alternating) (two-color-alternating-scsa current))
+        ((eql SCSA 'only-once) (only-once-scsa current))
         ;; more scsa's to come
         (T 0)))
