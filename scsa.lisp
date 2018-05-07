e9cf41d238383d90d2fa4de6039c532d80cfec8a
msats5 <saterson@gmail.com>
2018-05-07 18:01:53 -0400

    edits to scsa

--- scsa.lisp
+++ scsa.lisp
@@ -2,20 +2,18 @@
     ;; this is really dumb (only checks number of colors, not for alternating), so can be updated
     (let ((color-number (length (remove-duplicates current))))
         (if (> color-number 2)
-            ;; returns arbitrary bonus points (20) if current has no more than two colors
-            (return-from two-color-alternating-scsa 0)
-            (return-from two-color-alternating-scsa 10))))
+            (return-from two-color-alternating-scsa nil)
+            (return-from two-color-alternating-scsa T))))
 
 (defun only-once-scsa (current)
     ;; checks if the current element has duplicates or not
     (loop for e in current
         when (member e (rest (member e current)))
-        ;; returns arbitrary bonus points (20) if no duplicates
-        do (return-from only-once-scsa 0)
-        finally (return-from only-once-scsa 10)))
+        do (return-from only-once-scsa nil)
+        finally (return-from only-once-scsa T)))
 
 (defun scsa-match (current SCSA)
     (cond ((eql SCSA 'two-color-alternating) (two-color-alternating-scsa current))
         ((eql SCSA 'only-once) (only-once-scsa current))
         ;; more scsa's to come
-        (T 0)))
+        (T T)))
