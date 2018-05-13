62667abd1fa8708049fed16a83e2764f65d78c2e
guzmancesar <cesar.guzman61@myhunter.cuny.edu>
2018-05-13 13:17:31 -0400

    Update scsa.lisp

--- scsa.lisp
+++ scsa.lisp
@@ -113,3 +113,30 @@
 									 (setq w (append w nulist3))
 									 (setq x (append (list w) x)))))))
 		(return-from mystery-2-scsa x)))
+
+
+;;NOTE MYSTERY-5 USES RANDOM CHOOSER
+
+(defun random-chooser (list)
+  (nth (random (length list)) list))
+
+(defun mystery-5-scsa (population-size colors len)
+	(let ((x ())  (next 0) (randv 0) (choice ())) 
+			 (loop for q from 1 to population-size
+				do (setq choice ())
+				do (loop for q1 from 0 to len
+						 do (setq next (random-chooser colors))
+						; do (print "step: ") do (print q1)
+						 do (setq randv (random 1.0)) ;choose a random float from 0 - 1
+						; do (print randv)
+						 do (progn
+							(if (and (< randv .15) (< q1 (- len 3))) ;if less than .45 triple it 
+								 (progn (setq choice (append (list next) choice))(incf q1))) ;if .15 > randv .45 double it 
+							(if (and (< randv .45) (< q1 (- len 2)))
+								 (progn (setq choice (append (list next) choice)) 
+								 (setq choice (cons next choice)) (incf q1)  (incf q1)))
+							(if (< q1 len) (setq choice (append (list (random-chooser colors)) choice))))
+					;	do (print choice)
+					finally (setq x (cons choice x)))
+				;do (print x)
+				finally (return-from mystery-5-scsa x))))
